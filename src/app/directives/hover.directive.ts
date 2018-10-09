1df8f482e48a934f84106592e4b542d51739504f
djordjeprzulj <przulj@gmail.com>
2018-10-09 20:46:42 +0200

    Renderer2 umesto ElementRef, slajd 20.

--- src/app/directives/hover.directive.ts
+++ src/app/directives/hover.directive.ts
@@ -1,14 +1,17 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHover]'
 })
 export class HoverDirective implements OnInit {
 
-  constructor(private elementRef: ElementRef) {
+  constructor(private elementRef: ElementRef,
+              private renderer: Renderer2) {
   }
 
   ngOnInit() {
-    this.elementRef.nativeElement.style.backgroundColor = 'green';
+    this.renderer.setStyle(this.elementRef.nativeElement,
+                          'background-color',
+                          'green');
   }
 }
