badcb92ec2167f3456789de5a5065615c17ff54c
djordjeprzulj <przulj@gmail.com>
2018-10-09 20:58:30 +0200

    HostBinding, slajd 23.

--- src/app/directives/hover.directive.ts
+++ src/app/directives/hover.directive.ts
@@ -1,26 +1,24 @@
-import { Directive, ElementRef, OnInit, Renderer2, HostListener } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appHover]'
 })
 export class HoverDirective implements OnInit {
 
+  @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
+
   constructor(private elementRef: ElementRef,
               private renderer: Renderer2) {
   }
 
   ngOnInit() {
-    this.renderer.setStyle(this.elementRef.nativeElement,
-                          'background-color',
-                          'green');
   }
 
   @HostListener('mouseenter') mouseIn(eventData: Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'green');
+    this.backgroundColor = 'green';
   }
 
   @HostListener('mouseleave') mouseOut(eventData: Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
   }
-
 }
